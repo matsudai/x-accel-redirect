027e0bec297cc7b272cbd1ae49aa3a7eaf54d3b3
matsudai <matsudai.social.1131@gmail.com>
2022-10-24 14:59:45 +0000

    Update webapp

--- web/app/controllers/internal_files_controller.rb
+++ web/app/controllers/internal_files_controller.rb
@@ -1,6 +1,10 @@
 class InternalFilesController < ApplicationController
-  # GET /internal_files
+  # GET /internal_files?p=/path/to/file
   def index
-    render json: {}
+    render json: {
+      status: 200,
+      x_original_file_path: request.headers['X-Original-File-Path'],
+      file_path: params[:p]
+    }
   end
 end
